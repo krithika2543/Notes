a4b2839addc4b56536a98341ae299edc4e01dfaa
krithika2543 <133742234+krithika2543@users.noreply.github.com>
2026-06-18 16:06:22 +0530

    Update React.js

--- React.js
+++ React.js
@@ -1,5 +1,5 @@
 Instead of using traditional vanila tools like html, css, js for creating front end web pages, we will be using React.js. here instead of manually writing all the markup on html, we have a placeholder on html, reacy will directly fetch the markup from the jsx file and render it.
-To do this we have to create root and render the content into the root. 
+To do this we have to create root and render the content into the root. Its called root, because react has to render all the markups from js, so it better be in a central location called root.
 import {createRoot} from "react-dom/client";
 const root = createRoot(getElementById());
 root.render("some html tag");
